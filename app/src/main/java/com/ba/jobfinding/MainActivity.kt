ef4b37108486b975ec9e2a3b263a96fb4a2f3e1a
barq-al-layl <moh74s2001@gmail.com>
2023-07-17 11:39:22 +0200

    Initial commit

---
+++ app/src/main/java/com/ba/jobfinding/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ba.jobfinding
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.ba.jobfinding.ui.theme.JobFindingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JobFindingTheme {
+
+            }
+        }
+    }
+}
